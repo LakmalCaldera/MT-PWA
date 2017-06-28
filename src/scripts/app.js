81205af687126ebecfb4108d5cf19db1e177ff65
Prasad Mathugama <prasad.era@gmail.com>
2017-06-28 17:05:47 +0530

    init commit on push notifications

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,9 +1,15 @@
 
 
-  'use strict';
+'use strict';
 
-  $(document).ready(function(){
+$(document).ready(function () {
 
+  const applicationServerPublicKey = 'BBI9nw-rWN9YN59sLwSGq3p3EhoOlgacvvo_CmzVzTvmAeVZ3zkRz2iHPc50UxMRGE-i7By5VhleiuB99bxojJ4';
+
+  const pushButton = document.querySelector('.js-push-btn');
+
+  let isSubscribed = false;
+  let swRegistration = null;
   var app = {};
 
   /*****************************************************************************
@@ -11,17 +17,17 @@
    * Methods to update/refresh the UI
    *
    ****************************************************************************/
-   app.showBranchList = function(){
+  app.showBranchList = function () {
     $('#branch').show();
     $('#service').hide();
-   }
+  }
 
-   app.showServiceList = function(){
+  app.showServiceList = function () {
     $('#branch').hide();
     $('#service').show();
-   }
+  }
 
-   app.navigateToBranchList = function(branches){
+  app.navigateToBranchList = function (branches) {
     // update header
     app.updateHeader("Select a branch:");
 
@@ -30,23 +36,23 @@
 
     // Get template
     var template = Handlebars.compile($("#btn-template").html());
-    
+
     // Empty content
     $('#branch__list').html = "";
-    
+
     // Load in new data
-    branches.forEach(function(branch){
+    branches.forEach(function (branch) {
       $('#branch__list').append(template(branch));
     });
 
     // Setup Eventhandlers
-    $('.list-btn').on('click', function(event){
+    $('.list-btn').on('click', function (event) {
       app.getServices($(this).attr('data-id'), app.navigateToServiceList);
     });
   }
 
 
-  app.navigateToServiceList = function(services){
+  app.navigateToServiceList = function (services) {
     // update header
     app.updateHeader("Select a service:");
 
@@ -55,28 +61,28 @@
 
     // Get template
     var template = Handlebars.compile($("#btn-template").html());
-    
+
     // Empty content
     $('#service__list').html = "";
-    
+
     // Load in new data
-    services.forEach(function(service){
+    services.forEach(function (service) {
       $('#service__list').append(template(service));
     });
 
     // Setup Eventhandlers
-    $('.list-btn').on('click', function(event){
+    $('.list-btn').on('click', function (event) {
       alert('Clicked');
     });
   }
 
-  app.updateHeader = function(_title){
+  app.updateHeader = function (_title) {
     // Get template
     var template = Handlebars.compile($("#header-template").html());
-    $('#header').html(template({title: _title}));
+    $('#header').html(template({ title: _title }));
 
-    $('#butRefresh').on('click', function() {
-    // Refresh all of the forecasts
+    $('#butRefresh').on('click', function () {
+      // Refresh all of the forecasts
       app.getBranches();
     });
 
@@ -88,7 +94,7 @@
    *
    ****************************************************************************/
 
-  app.getBranches = function(callback) {
+  app.getBranches = function (callback) {
     var url = "/geo/branches/?longitude=0&latitude=0&radius=2147483647";
     if ('caches' in window) {
       /*
@@ -96,7 +102,7 @@
        * If the service worker has the data, then display the cached
        * data while the app fetches the latest data.
        */
-      caches.match(url).then(function(response) {
+      caches.match(url).then(function (response) {
         if (response) {
           response.json().then(function updateFromCache(data) {
             callback(data);
@@ -106,7 +112,7 @@
     }
     // Fetch the latest data.
     var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
+    request.onreadystatechange = function () {
       if (request.readyState === XMLHttpRequest.DONE) {
         if (request.status === 200) {
           var data = JSON.parse(request.response);
@@ -119,15 +125,15 @@
   };
 
 
-  app.getServices = function(branchId, callback) {
-    var url = "MobileTicket/branches/"+ branchId +"/services/wait-info";
+  app.getServices = function (branchId, callback) {
+    var url = "MobileTicket/branches/" + branchId + "/services/wait-info";
     if ('caches' in window) {
       /*
        * Check if the service worker has already cached this url
        * If the service worker has the data, then display the cached
        * data while the app fetches the latest data.
        */
-      caches.match(url).then(function(response) {
+      caches.match(url).then(function (response) {
         if (response) {
           response.json().then(function updateFromCache(data) {
             callback(data);
@@ -137,11 +143,11 @@
     }
     // Fetch the latest data.
     var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
+    request.onreadystatechange = function () {
       if (request.readyState === XMLHttpRequest.DONE) {
         if (request.status === 200) {
           var data = JSON.parse(request.response);
-          data.forEach(function(item){
+          data.forEach(function (item) {
             item.id = item.serviceId;
             item.name = item.serviceName;
           });
@@ -156,11 +162,142 @@
 
   app.getBranches(app.navigateToBranchList);
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./service-worker.js')
-             .then(function() { console.log('Service Worker Registered'); });
+  if ('serviceWorker' in navigator && 'PushManager' in window) {
+    console.log('Service Worker and Push is supported');
+
+
+    navigator.serviceWorker.register('./service-worker.js')
+      .then(function (swReg) {
+        console.log('Service Worker is registered', swReg);
+
+        swRegistration = swReg;
+        initialiseUI();
+      })
+      .catch(function (error) {
+        console.error('Service Worker Error', error);
+      });
+  } else {
+    console.warn('Push messaging is not supported');
+    pushButton.textContent = 'Push Not Supported';
   }
 
-  });
+  function initialiseUI() {
+    // Set the initial subscription value
+    swRegistration.pushManager.getSubscription()
+      .then(function (subscription) {
+        isSubscribed = !(subscription === null);
+
+        if (isSubscribed) {
+          console.log('User IS subscribed.');
+        } else {
+          console.log('User is NOT subscribed.');
+        }
+
+        updateBtn();
+      });
+
+    pushButton.addEventListener('click', function () {
+      pushButton.disabled = true;
+      if (isSubscribed) {
+        unsubscribeUser();
+      } else {
+        subscribeUser();
+      }
+    });
+  }
+
+  function subscribeUser() {
+    const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+    swRegistration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: applicationServerKey
+    })
+      .then(function (subscription) {
+        console.log('User is subscribed.');
+
+        updateSubscriptionOnServer(subscription);
+
+        isSubscribed = true;
+
+        updateBtn();
+      })
+      .catch(function (err) {
+        console.log('Failed to subscribe the user: ', err);
+        updateBtn();
+      });
+  }
+
+  function unsubscribeUser() {
+    swRegistration.pushManager.getSubscription()
+      .then(function (subscription) {
+        if (subscription) {
+          return subscription.unsubscribe();
+        }
+      })
+      .catch(function (error) {
+        console.log('Error unsubscribing', error);
+      })
+      .then(function () {
+        updateSubscriptionOnServer(null);
+
+        console.log('User is unsubscribed.');
+        isSubscribed = false;
+
+        updateBtn();
+      });
+  }
+
+
+  function updateSubscriptionOnServer(subscription) {
+    // TODO: Send subscription to application server
+
+    const subscriptionJson = document.querySelector('.js-subscription-json');
+    const subscriptionDetails =
+      document.querySelector('.js-subscription-details');
+
+    if (subscription) {
+      subscriptionJson.textContent = JSON.stringify(subscription);
+      subscriptionDetails.classList.remove('is-invisible');
+    } else {
+      subscriptionDetails.classList.add('is-invisible');
+    }
+  }
+
+
+  function updateBtn() {
+    if (Notification.permission === 'denied') {
+      pushButton.textContent = 'Push Messaging Blocked.';
+      pushButton.disabled = true;
+      updateSubscriptionOnServer(null);
+      return;
+    }
+
+    if (isSubscribed) {
+      pushButton.textContent = 'Disable Push Messaging';
+    } else {
+      pushButton.textContent = 'Enable Push Messaging';
+    }
+
+    pushButton.disabled = false;
+  }
+
+
+
+  function urlB64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+      .replace(/\-/g, '+')
+      .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  }
+
+
+});
 
